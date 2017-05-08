2c43a3307f03948d0ad7633cebe7354cbb6f413e
sharanczaaa <michal.keska.94@gmail.com>
2017-05-08 22:57:17 +0000

    dodanie username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508154138) do
+ActiveRecord::Schema.define(version: 20170508224849) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170508154138) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
